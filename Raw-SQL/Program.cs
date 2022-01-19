9688eda6f2ccf10bb24ec61248e6f2a65825981c
hiwad rashad <hiwadrashad112@hotmail.com>
2022-01-19 17:06:57 +0100

    Added nested entity writing

--- Raw-SQL/Program.cs
+++ Raw-SQL/Program.cs
@@ -10,6 +10,7 @@ namespace Raw_SQL
     {
         public static string connectionstring = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=RawSqlDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
+
         public static void AddModel(Child child1)
         {
             SqlConnection MainConnection = new SqlConnection(connectionstring);
@@ -18,6 +19,99 @@ namespace Raw_SQL
             SqlCommand SqlCommand = new SqlCommand(Command, MainConnection);
             SqlCommand.ExecuteNonQuery();
             MainConnection.Close();
+        }
+
+        public static void AddModelParent(Parent parent1)
+        {
+            try
+            {
+                AddModel(parent1.Child);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            SqlConnection MainConnection = new SqlConnection(connectionstring);
+            MainConnection.Open();
+            string Command = "Insert Into [ParentTable] (Id,ChildId,Text) values ('" + parent1.Id + "','" + parent1.Child.Id + "','" + parent1.Text + "')";
+            SqlCommand SqlCommand = new SqlCommand(Command, MainConnection);
+            SqlCommand.ExecuteNonQuery();
+            MainConnection.Close();
+        }
+
+        public static Child GetChildById(Guid Id)
+        {
+            SqlConnection MainConnection = new SqlConnection(connectionstring);
+            MainConnection.Open();
+            string Command = "Select Id, Text from [Table]" + "Where Id = '" + Id.ToString() + "'";
+            SqlCommand SqlCommand = new SqlCommand(Command, MainConnection);
+            var myReader = SqlCommand.ExecuteReader();
+            List<Child> children2 = new List<Child>();
+            while (myReader.Read())
+            {
+                var child2 = new Child();
+                child2.Id = new Guid(myReader.GetValue(0).ToString());
+                child2.Text = myReader.GetValue(1).ToString();
+                children2.Add(child2);
+            }
+            MainConnection.Close();
+            return children2.FirstOrDefault();
+        }
+
+        public static Parent GetParentById(Guid Id)
+        {
+            SqlConnection MainConnection = new SqlConnection(connectionstring);
+            MainConnection.Open();
+            string Command = "Select Id,ChildId, Text from [ParentTable]" + "Where Id = '" + Id.ToString() + "'";
+            SqlCommand SqlCommand = new SqlCommand(Command, MainConnection);
+            var myReader = SqlCommand.ExecuteReader();
+            List<Parent> parents = new List<Parent>();
+            while (myReader.Read())
+            {
+                var parent = new Parent();
+                parent.Id = new Guid(myReader.GetValue(0).ToString());
+                try
+                {
+                    parent.Child = GetChildById(new Guid(myReader.GetValue(1).ToString()));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    break;
+                }
+                parent.Text = myReader.GetValue(2).ToString();
+                parents.Add(parent);
+            }
+            MainConnection.Close();
+            return parents.FirstOrDefault();
+        }
+
+        public static List<Parent> GetAllParents()
+        {
+            SqlConnection MainConnection = new SqlConnection(connectionstring);
+            MainConnection.Open();
+            string Command = "Select * from [ParentTable]";
+            SqlCommand SqlCommand = new SqlCommand(Command, MainConnection);
+            var myReader = SqlCommand.ExecuteReader();
+            List<Parent> parents = new List<Parent>();
+            while (myReader.Read())
+            {
+                var parent = new Parent();
+                parent.Id = new Guid(myReader.GetValue(0).ToString());
+                try
+                {
+                    parent.Child = GetChildById(new Guid(myReader.GetValue(1).ToString()));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    break;
+                }
+                parent.Text = myReader.GetValue(2).ToString();
+                parents.Add(parent);
+            }
+            MainConnection.Close();
+            return parents;
         }
 
         public static List<Child> GetAllChildren()
@@ -48,6 +142,16 @@ namespace Raw_SQL
             SqlCommand.ExecuteNonQuery();
             MainConnection.Close();
         }
+
+        public static void DeleteChild(Child child)
+        {
+            SqlConnection MainConnection = new SqlConnection(connectionstring);
+            MainConnection.Open();
+            string Command = "Delete from [Table] where Id='" + child.Id + "'";
+            SqlCommand SqlCommand = new SqlCommand(Command, MainConnection);
+            SqlCommand.ExecuteNonQuery();
+            MainConnection.Close();
+        }
         static void Main(string[] args)
         {
             var child1 = new Child()
@@ -69,17 +173,16 @@ namespace Raw_SQL
                 },
                 Text = "From GrandParent"
             };
-            SqlConnection MainConnection = new SqlConnection(connectionstring);
-            MainConnection.Open();
-            var child = GetAllChildren().FirstOrDefault();
-            string Command = "Delete from [Table] where ";
-            SqlCommand SqlCommand = new SqlCommand(Command, MainConnection);
-            SqlCommand.ExecuteNonQuery();
-            MainConnection.Close();
-            foreach (var item in children)
-            {
-                Console.WriteLine($"id : {item.Id}  text : {item.Text}");
-            }
+
+
+            var item = GetParentById(GetAllParents().FirstOrDefault().Id);
+
+            Console.WriteLine($"id : {item.Id}  text : {item.Text} childid : {item.Child.Id}");
+
+            //foreach (var item in children2)
+            //{
+            //    Console.WriteLine($"id : {item.Id}  text : {item.Text}");
+            //}
         }
     }
 }
